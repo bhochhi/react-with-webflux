c09a860b56bb1d907b67dd163e208ce9cd83b147
Rupesh Bhochhibhoya <bhochhi@gmail.com>
2019-01-17 14:12:38 -0600

    making multiple ajax calls throught rxjs

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,126 +1,155 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AccountSection from './components/account-section.component';
 import ActivitySection from './components/activity-section.component';
+import {
+  range,
+  Observable,
+  combineLatest
+} from 'rxjs';
+
+import {Observable as Obs} from 'rxjs/Observable'
+import {
+  map,
+  filter
+} from 'rxjs/operators';
+import {
+  ajax
+} from 'rxjs/ajax';
 
 class App extends Component {
   constructor(props, context) {
     super(props, context);
 
     this.state = {
-        bankAccounts: ["bankAccounts1", "bankAccounts2"],
-        insuranceAccounts: ["insuranceAccounts1", "insuranceAccounts2"],
-        transactions: ["transaction1","transaction2"]
+      bankAccounts: ["bankAccounts1", "bankAccounts2"],
+      insuranceAccounts: ["insuranceAccounts1", "insuranceAccounts2"],
+      transactions: ["transaction1", "transaction2"]
     };
   }
-   
-  
-  componentDidMount(){
-    
-    let bank = [
-      {
-        name:'bankAc1'      
+
+
+  componentDidMount() {
+
+    const bank = [{
+        name: 'bankAc1'
       },
       {
-        name:'bankAc2'      
+        name: 'bankAc2'
       },
       {
-        name:'bankAc3'      
+        name: 'bankAc3'
       },
       {
-        name:'bankAc4'      
+        name: 'bankAc4'
       }
     ]
-    let insurance = [
-      {
-        name:'insurance1'      
+    const insurance = [{
+        name: 'insurance1'
       },
       {
-        name:'insurance2'      
+        name: 'insurance2'
       },
       {
-        name:'insurance3'      
+        name: 'insurance3'
       },
       {
-        name:'insurance4'      
+        name: 'insurance4'
       }
     ]
 
-    let trans = [
-      {
-        name:'trans1 desc'      
+    let trans = [{
+        name: 'trans1 desc'
       },
       {
-        name:'trans2 desc'      
+        name: 'trans2 desc'
       },
       {
-        name:'trans3 desc'      
+        name: 'trans3 desc'
       },
       {
-        name:'trans4 desc'      
+        name: 'trans4 desc'
       }
     ]
 
-    fetch("https://httpbin.org/anything/insurance", {
-      method: 'POST',
-      mode:'cors',
-      body: JSON.stringify(insurance), 
-      headers:{
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(res => new Promise(resolve => setTimeout(() => resolve(res.json()), 1000)))
-    .then(respone=>{
-      let accountData = respone.json.map(e=>e.name)
-      console.log('accountData',accountData);
-      this.setState({insuranceAccounts:accountData});
-    })
-    .catch(err=> console.log(err));
 
-    fetch("https://httpbin.org/anything/banking", {
-      method: 'POST',
-      mode:'cors',
-      body: JSON.stringify(bank), 
-      headers:{
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(res => new Promise(resolve => setTimeout(() => resolve(res.json()), 3000)))
-    .then(respone=>{
-      let accountData = respone.json.map(e=>e.name)
-      console.log('accountData',accountData);
-      this.setState({bankAccounts:accountData});
-    })
-    .catch(err=> console.log(err));
 
-    fetch("https://httpbin.org/anything/transactions", {
+    const banking$ = ajax({
+      url: 'https://httpbin.org/post',
       method: 'POST',
-      mode:'cors',
-      body: JSON.stringify(trans), 
-      headers:{
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(res => new Promise(resolve => setTimeout(() => resolve(res.json()), 4000)))
-    .then(respone=>{
-      let accountData = respone.json.map(e=>e.name)
-      console.log('accountData',accountData);
-      this.setState({transactions:accountData});
-    })
-    .catch(err=> console.log(err));
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: bank
+    });
+
+    const insurance$ = ajax({
+      url: 'https://httpbin.org/post',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: insurance
+    });
+
+    const transactions$ = ajax({
+      url: 'https://httpbin.org/post',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: trans
+    });
+
+    const subscription = combineLatest(banking$, insurance$, transactions$)
+      .subscribe(([bankRes, insuranceRes,transRes]) => {
+        let bankAccountData = bankRes.response.json.map(e => e.name);
+        let insuranceAccountData = insuranceRes.response.json.map(e => e.name);
+        let transactonData = transRes.response.json.map(e => e.name);
+
+        //simulating delay
+        setTimeout(() => {
+          this.setState({
+            bankAccounts: bankAccountData
+          });
+        }, 4000);
+        setTimeout(() => this.setState({
+          insuranceAccounts: insuranceAccountData
+        }), 3000);
+        setTimeout(() => this.setState({
+          transactions: transactonData
+        }), 1000);
+
+      }, e => console.error(e));
 
   }
+
+  
 
 
   render() {
-    return (
-      <div className="App">
-        <AccountSection bankAccounts={this.state.bankAccounts} insuranceAccounts={this.state.insuranceAccounts} />
-        <ActivitySection bills={this.state.bills} transactions={this.state.transactions} />
+    return ( 
+    <div className = "App" >
+      <AccountSection bankAccounts = {
+        this.state.bankAccounts
+      }
+      insuranceAccounts = {
+        this.state.insuranceAccounts
+      }
+      /> 
+      <hr />
+      <ActivitySection bills = {
+        this.state.bills
+      }
+      transactions = {
+        this.state.transactions
+      }
+      /> 
       </div>
     );
   }
 }
-
 export default App;
