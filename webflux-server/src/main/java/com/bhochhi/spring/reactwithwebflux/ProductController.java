24892fe349657ed82d253636008ad35f1547c3e0
Rupesh Bhochhibhoya <bhochhi@gmail.com>
2019-01-17 09:29:37 -0600

    work in progress

---
+++ webflux-server/src/main/java/com/bhochhi/spring/reactwithwebflux/ProductController.java
@@ -0,0 +1,42 @@
+package com.bhochhi.spring.reactwithwebflux;
+
+import org.reactivestreams.Publisher;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/products", produces = MediaType.APPLICATION_JSON_VALUE)
+public class ProductController {
+
+
+    private ProductRepository productRepository;
+    private ApplicationEventPublisher publisher;
+
+    ProductController(ProductRepository productRepository, ApplicationEventPublisher publisher) {
+        this.productRepository = productRepository;
+        this.publisher = publisher;
+    }
+
+
+    @PostMapping
+    Publisher<ResponseEntity<Product>> create(@RequestBody Product product) {
+        return this.productRepository
+                .save(product)
+                .doOnSuccess(prod -> this.publisher.publishEvent(prod))
+                .map(p -> ResponseEntity.created(URI.create("/products/" + p.getId()))
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .build());
+    }
+
+
+    @GetMapping
+    Publisher<Product> getAll() {
+        return this.productRepository.findAll();
+
+    }
+
+}
