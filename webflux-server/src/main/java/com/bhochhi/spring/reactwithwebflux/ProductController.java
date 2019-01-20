fb7276b1d6303bb74e0fdca6614144b0f0f5c226
Rupesh Bhochhibhoya <bhochhi@gmail.com>
2019-01-20 12:59:15 -0600

    wip: server adding api that publishes the events with data

--- webflux-server/src/main/java/com/bhochhi/spring/reactwithwebflux/ProductController.java
+++ webflux-server/src/main/java/com/bhochhi/spring/reactwithwebflux/ProductController.java
@@ -1,25 +1,31 @@
 package com.bhochhi.spring.reactwithwebflux;
 
+import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Publisher;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 
 import java.net.URI;
 
+@Slf4j
 @RestController
-@RequestMapping(value = "/products", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/products")
 @CrossOrigin(origins = "*")
 public class ProductController {
 
 
     private ProductRepository productRepository;
     private ApplicationEventPublisher publisher;
+    private WebClient   webClient;
 
-    ProductController(ProductRepository productRepository, ApplicationEventPublisher publisher) {
+    ProductController(ProductRepository productRepository, ApplicationEventPublisher publisher, WebClient webClient) {
         this.productRepository = productRepository;
         this.publisher = publisher;
+        this.webClient = webClient;
     }
 
 
@@ -40,9 +46,23 @@ public class ProductController {
 
     }
 
+    @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    Publisher<Product> getAllStream() {
+
+        //make webclient call to three services...
+
+
+        return this.productRepository.findAll();
+    }
+
 
     @GetMapping(value = "/banking")
     Publisher<Product> getAllBanking() {
+        return this.productRepository.findAllByType("bank");
+    }
+    @GetMapping(value = "/banking/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    Flux<Product> getAllBankingStream() {
+        log.info("publishing stream");
         return this.productRepository.findAllByType("bank");
 
     }
